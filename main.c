a79629d3f6ab6207c681b4872c0611d810c97eaa
CSEE-student <57561345+CSEE-student@users.noreply.github.com>
2019-11-27 20:16:16 +0900

    test hw3

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    printf("1");
-    printf("5");
-    printf("67863915");
+    printf("1 \n");
+    printf("5 \n");
+    printf("67863915 \n");
     return 0;
 }
